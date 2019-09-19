48e7a1d0c9feffd8bdea41a7d3ad817e869307bc
Joerg Rossdeutscher <j.rossdeutscher@superreal.de>
2019-09-19 20:43:53 +0200

    init

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    App\JoergRossdeutscher\SpookyBundle\JoergRossdeutscherSpookyBundle::class => ['all' => true],
+];
---
+++ src/JoergRossdeutscher/SpookyBundle/JoergRossdeutscherSpookyBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\JoergRossdeutscher\SpookyBundle;
+#namespace App\JoergR Acme\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JoergRossdeutscherSpookyBundle extends Bundle
+{
+}
